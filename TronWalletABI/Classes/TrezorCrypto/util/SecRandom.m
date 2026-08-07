1d9b707ab57ea2960603fa4f54ef027c7c7ae553
elonGT <elon.xu@tron.network>
2026-08-07 21:02:03 +0800

    Abort on secure random generation failure

--- TronWalletABI/Classes/TrezorCrypto/util/SecRandom.m
+++ TronWalletABI/Classes/TrezorCrypto/util/SecRandom.m
@@ -1,14 +1,20 @@
 @import Foundation;
 @import Security;
 
+#include <stdlib.h>
+
 uint32_t random32(void) {
     uint32_t value;
     int status = SecRandomCopyBytes(kSecRandomDefault, sizeof(value), &value);
-    NSCAssert(status == errSecSuccess, @"Failed to generate random number");
+    if (status != errSecSuccess) {
+        abort();
+    }
     return value;
 }
 
 void random_buffer(uint8_t *buf, size_t len) {
     int status = SecRandomCopyBytes(kSecRandomDefault, len, buf);
-    NSCAssert(status == errSecSuccess, @"Failed to generate random number");
+    if (status != errSecSuccess) {
+        abort();
+    }
 }
